49a3b24879c6d183f1382e689913a9993e3c1694
Thiago <thiagooffice2@gmail.com>
2022-01-12 22:48:58 -0300

    Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import {FiSearch} from 'react-icons/fi'
+import './styles.css'
+import api from './services/api'
+
+
 
 function App() {
+
+  const [input, setInput] = useState("")
+
+  async function handleSearch(){
+    // 01310930/json/
+
+    if(input === ""){
+      alert("Preencha algum cep!")
+      return
+    }
+
+    try{
+      const response = await api.get(`${input}/json`)
+      console.log(response.data)
+    }catch{
+      alert("Ops erro ao buscar")
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">
+        Buscador CEP
+      </h1>
+  
+    <div className="containerInput">
+      <input type="text"
+      placeholder="Digite seu cep..."
+      value={input}
+      onChange={(e)=> setInput(e.target.value) }
+      />
+
+      <button className="buttonSearch">
+        <FiSearch size={25} color="fff"
+        onClick={handleSearch}
+        />
+      </button>
+    </div>
+
+    <main className='main'>
+      <h2>CEP: 78747784</h2>
+      
+
+      <span>Rua Teste algum</span>
+      <span>Complemento; algum complemento</span>
+      <span>Vila Rosa</span>
+      <span>Campo Grande - MS</span>
+    </main>
+
+
     </div>
   );
 }
